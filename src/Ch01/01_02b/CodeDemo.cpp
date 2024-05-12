e1178b5728537ca2695073df9374b1aad783b133
Craig Steele <CraigRSteele@users.noreply.github.com>
2024-05-12 23:39:52 +0000

    Hello World

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,20 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Enter your name: " << std::flush;
+
+    std::string name;
+    std::cin >> name; 
+
+    
+    std::cout << "Hello " << name << "!" << std::endl;
+
+    std::cout << std::endl << std::endl;
+    return (0);
+}
